f2a29f276f35c5c09512b9f6bfb56d9ff88c00ae
Bhbored <bhbored2022@gmail.com>
2026-04-07 19:19:14 +0300

    done with education

--- Portfolio/Models/Education.cs
+++ Portfolio/Models/Education.cs
@@ -6,6 +6,5 @@
         public string Title { get; set; } = string.Empty;
         public string Issuer { get; set; } = string.Empty;
         public string Year { get; set; } = string.Empty;
-        public string? Link { get; set; }
     }
 }
